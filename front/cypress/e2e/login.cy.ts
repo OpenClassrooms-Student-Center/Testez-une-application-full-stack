988fb44ab416ef7976475691d057c1d4ab71c4c1
Joffrey Hernandez <joffrey.hernandez@gmail.com>
2022-10-17 01:44:57 +0200

    add cypress example for intercept data

---
+++ front/cypress/e2e/login.cy.ts
@@ -0,0 +1,27 @@
+describe('Login spec', () => {
+  it('Login successfull', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true
+      },
+    })
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.url().should('include', '/sessions')
+  })
+});
