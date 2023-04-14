140b0b5a0af036dc18b7757114864a2e5a5f1cd2
VelosoTonym <veloso.tony@gmail.com>
2023-04-14 20:44:57 +0200

    fix cypress config

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'iya2j9',
@@ -9,9 +9,10 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
+    testIsolation: false,
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
+      return require('./cypress/plugins/index.ts').default(on, config);
     },
     baseUrl: 'http://localhost:4200',
   },
-})
+});
