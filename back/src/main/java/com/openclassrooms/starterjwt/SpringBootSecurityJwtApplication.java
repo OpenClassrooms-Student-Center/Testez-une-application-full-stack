68f899ff6eb0ce7035fc5b34a76536cb2eb1075b
VelosoTony <veloso.tony@gmail.com>
2023-04-27 13:12:37 +0200

    config lombok generated

--- back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
+++ back/src/main/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import lombok.Generated;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringBootSecurityJwtApplication {
+
+	@Generated
 	public static void main(String[] args) {
-    SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
 	}
 }
