ec3b6a0bb6f7b243caf332f921857b3e71d2c58c
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-08-14 12:08:16 +0200

    :passport_control: Authentication controller test modification passed

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -1,7 +1,5 @@
 package com.openclassrooms.starterjwt.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.request.SignupRequest;
@@ -16,69 +14,62 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.time.Instant;
-import java.util.Date;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthControllerTest {
     @InjectMocks
     AuthController authController;
-    @Autowired
-    private MockMvc mockMvc; //Pour les appeles REST
-    @Mock //Mock les services
+    @Mock
     private AuthenticationManager authenticationManagerMock;
 
-    @Mock //Mock les services
+    @Mock
     private JwtUtils jwtUtilsMock;
 
-    @Mock //Mock les services
+    @Mock
     private UserRepository userRepositoryMock;
 
-    @Mock //Mock les services
-    private PasswordEncoder  passwordEncoder;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    SecurityContext securityContext;
+
+    @Mock
+    Authentication authentication;
 
     @BeforeEach
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+        authController = new AuthController(
+                authenticationManagerMock,
+                passwordEncoder,
+                jwtUtilsMock,
+                userRepositoryMock);
     }
-
 
     @Test
     @DisplayName("Login test")
     void whenUserIsAuthenticated_thenReturnJwtResponse() throws Exception {
+        //GIVEN
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setEmail("yoga@studio.com");
         loginRequest.setPassword("test!1234");
 
-        Authentication authentication = mock(Authentication.class);
-
-        when(authenticationManagerMock.authenticate(any(UsernamePasswordAuthenticationToken.class)))
-                .thenReturn(authentication);
         String token = "Test-token";
-        when(jwtUtilsMock.generateJwtToken(authentication)).thenReturn(token);
 
         UserDetailsImpl userDetails = UserDetailsImpl.builder()
                 .id(1L) // Assuming id is Long
@@ -88,92 +79,144 @@ public class AuthControllerTest {
                 .firstName("Admin")
                 .lastName("ADMIN")
                 .build();
-        when(authentication.getPrincipal()).thenReturn(userDetails);
 
-        User user = User.builder()
-                .id(userDetails.getId())
-                .admin(userDetails.getAdmin())
-                .email(userDetails.getUsername())
-                .password(userDetails.getPassword())
-                .firstName(userDetails.getFirstName())
-                .lastName(userDetails.getLastName())
-                .build();
+        User user = new User().setAdmin(true);
+        boolean isAdmin = user.isAdmin();
+        //WHEN
+        when(authenticationManagerMock.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+
+        SecurityContextHolder.setContext(securityContext);
+        securityContext.setAuthentication(authentication);
+
+        when(jwtUtilsMock.generateJwtToken(authentication)).thenReturn(token);
+
+        when(authentication.getPrincipal()).thenReturn(userDetails);
 
         when(userRepositoryMock.findByEmail(userDetails.getUsername()))
                 .thenReturn(Optional.of(user));
 
+        //THEN
+        JwtResponse jwtResponse = new JwtResponse(
+                token,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getFirstName(),
+                userDetails.getLastName(),
+                isAdmin);
 
-        mockMvc.perform(post("/api/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(loginRequest)))
-                .andExpect(jsonPath("$.token").value("Test-token"))
-                .andExpect(jsonPath("$.type").value("Bearer"))
-                .andExpect(jsonPath("$.id").value(1)) // Match the same data type as id in UserDetailsImpl
-                .andExpect(jsonPath("$.username").value("yoga@studio.com"))
-                .andExpect(jsonPath("$.firstName").value("Admin"))
-                .andExpect(jsonPath("$.lastName").value("ADMIN"))
-                .andExpect(jsonPath("$.admin").value(true)).andReturn();
+        ResponseEntity<?> logged = ResponseEntity.ok(jwtResponse);
+        ResponseEntity<JwtResponse> login = (ResponseEntity<JwtResponse>) authController.authenticateUser(loginRequest);
+
+        assertEquals("You are logged", logged.getStatusCode(), login.getStatusCode());
+        assertThat(login.getBody().getId()).isEqualTo(1L);
+        assertThat(login.getBody().getAdmin()).isTrue();
+        assertThat(login.getBody().getToken()).isEqualTo("Test-token");
+        assertThat(login.getBody().getFirstName()).isEqualTo("Admin");
+        assertThat(login.getBody().getLastName()).isEqualTo("ADMIN");
+        assertThat(login.getBody().getUsername()).isEqualTo("yoga@studio.com");
+
+        verify(authenticationManagerMock, times(1))
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        verify(jwtUtilsMock, times(1)).generateJwtToken(authentication);
+
+        verify(userRepositoryMock, times(1)).findByEmail(userDetails.getUsername());
     }
 
-
     @Test
-    @DisplayName("Error for empty required field")
-    void whenEmptyRequiredField_thenReturnAnError(){
-        LoginRequest invalidRequest = new LoginRequest();
-        invalidRequest.setEmail("");
-        invalidRequest.setPassword("test!1234");
+    @DisplayName("User is null")
+    void whenUserIsNull_isAdminIsFalse() {
+        //GIVEN
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("test@invalid.com");
+        loginRequest.setPassword("invalid!1234");
 
-        Authentication authentication = mock(Authentication.class);
+        String token = "Test-token";
+
         UserDetailsImpl userDetails = UserDetailsImpl.builder()
-                .username(invalidRequest.getEmail())
-                .password(invalidRequest.getPassword())
+                .username(loginRequest.getEmail())
+                .password(loginRequest.getPassword())
                 .build();
 
+        //WHEN
         when(authenticationManagerMock.authenticate(any(UsernamePasswordAuthenticationToken.class)))
                 .thenReturn(authentication);
 
-        authenticationManagerMock.authenticate(new UsernamePasswordAuthenticationToken(
-                invalidRequest.getEmail(), invalidRequest.getPassword()));
+        SecurityContextHolder.setContext(securityContext);
+        securityContext.setAuthentication(authentication);
 
-        Optional<User> invalidEmailExpected = userRepositoryMock.findByEmail(userDetails.getUsername());
+        when(jwtUtilsMock.generateJwtToken(authentication)).thenReturn(token);
 
-        assertThat(invalidEmailExpected).isEqualTo(Optional.empty());
+        when(authentication.getPrincipal()).thenReturn(userDetails);
 
+        //THEN
+        User user = userRepositoryMock.findByEmail(userDetails.getUsername()
+        ).orElse(null);
+
+        ResponseEntity<JwtResponse> login = (ResponseEntity<JwtResponse>) authController.authenticateUser(loginRequest);
+
+        assertThat(user).isNull();
+        assertThat(login.getBody().getAdmin()).isFalse();
+        verify(authenticationManagerMock, times(1))
+                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        verify(jwtUtilsMock, times(1)).generateJwtToken(authentication);
+
+        verify(userRepositoryMock, times(2)).findByEmail(userDetails.getUsername());
     }
+
     @Test
     @DisplayName("Register test")
-    void whenSignupRequestIsValid_thenSaveUser() throws Exception {
+    void whenSignupRequestIsValid_thenSaveUser() {
+        //GIVEN
         SignupRequest signupRequest = new SignupRequest();
         signupRequest.setEmail("guerda@test.com");
         signupRequest.setFirstName("Guerda");
         signupRequest.setLastName("GUERDA");
         signupRequest.setPassword("test!1234");
 
-        when(passwordEncoder.encode(any())).thenReturn(signupRequest.getPassword());
-
         User user = new User(signupRequest.getEmail(),
                 signupRequest.getLastName(),
                 signupRequest.getFirstName(),
                 signupRequest.getPassword(),
                 false);
+        //WHEN
+        when(passwordEncoder.encode(any())).thenReturn(user.getPassword());
 
         when(userRepositoryMock.save(any(User.class)))
                 .thenReturn(user);
+        //THEN
+        MessageResponse messageResponse = new MessageResponse("User registered successfully!");
 
-        new MessageResponse("User registered successfully!");
+        ResponseEntity<?> messageSent = ResponseEntity.ok(messageResponse);
 
-        mockMvc.perform(post("/api/auth/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(signupRequest)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("User registered successfully!"))
-                .andReturn();
+        ResponseEntity<MessageResponse> sendMessage = (ResponseEntity<MessageResponse>) authController.registerUser(signupRequest);
+
+        assertEquals("Returned message", messageSent.getStatusCode(), sendMessage.getStatusCode());
+        assertThat(sendMessage.getBody().getMessage()).isEqualTo(messageResponse.getMessage());
+        verify(passwordEncoder, times(1)).encode(user.getPassword());
+        verify(userRepositoryMock, times(1)).save(user);
     }
 
-    // RAF
-    //LOGIN :
-    //La gestion des erreurs en cas de mauvais login / password
+    @Test
+    @DisplayName("BAD REQUEST")
+    void whenEmailAlreadyExists_thenReturnBadRequest() {
+        //GIVEN
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setEmail("guerda@test.com");
+        //WHEN
+        when(userRepositoryMock.existsByEmail(signupRequest.getEmail()))
+                .thenReturn(true);
+        //THEN
+        MessageResponse messageResponse = new MessageResponse("Error: Email is already taken!");
+        ResponseEntity<?> badRequest = ResponseEntity.badRequest()
+                .body(messageResponse);
 
-    //REGISTER:
-    //L’affichage d’erreur en l’absence d’un champ obligatoire
+        ResponseEntity<MessageResponse> email = (ResponseEntity<MessageResponse>) authController.registerUser(signupRequest);
+
+        assertThat(email.getStatusCode()).isEqualTo(badRequest.getStatusCode());
+        assertThat(email.getBody().getMessage()).isEqualTo(messageResponse.getMessage());
+        verify(userRepositoryMock, times(1)).existsByEmail(signupRequest.getEmail());
     }
+}
