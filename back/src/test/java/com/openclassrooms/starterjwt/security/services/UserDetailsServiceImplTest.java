ae3e015f6cbd46214227bfaede128dd035d34bb0
Marvin Silva <dejesusdasilva.marvin@gmail.com>
2023-08-31 22:27:12 +0200

    :white_check_mark: Implementation du Service des details des utilisateurs tested

--- back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsServiceImplTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsServiceImplTest.java
@@ -12,8 +12,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -36,7 +38,16 @@ public class UserDetailsServiceImplTest {
     void whenUsernameIsFound_thenReturnUserDetailsImpl(){
 
         String username = "test@test.com";
-        User user = new User();
+        User user = User.builder()
+                .id(30L)
+                .firstName("Kenneth")
+                .lastName("Lechat")
+                .email(username)
+                .password("test!1234")
+                .admin(true)
+                .createdAt(LocalDateTime.parse("2023-08-31T15:20:00"))
+                .updatedAt(LocalDateTime.parse("2023-08-31T15:20:00"))
+                .build();
 
         when(userRepository.findByEmail(username))
                 .thenReturn(Optional.of(user));
@@ -53,23 +64,34 @@ public class UserDetailsServiceImplTest {
                 userDetailsServiceImpl.loadUserByUsername(username);
 
         assertEquals("UserDetails", userDetails, requestUserDetails);
+        assertThat(userDetails.getAdmin()).isNull();
+        assertThat(userDetails.getAuthorities()).isEqualTo(requestUserDetails.getAuthorities());
+        assertThat(userDetails.getPassword()).isEqualTo(requestUserDetails.getPassword());
+        assertThat(userDetails.getUsername()).isEqualTo(requestUserDetails.getUsername());
+        assertThat(userDetails.isAccountNonExpired()).isEqualTo(requestUserDetails.isAccountNonExpired());
+        assertThat(userDetails.isAccountNonLocked()).isEqualTo(requestUserDetails.isAccountNonLocked());
+        assertThat(userDetails.isAccountNonExpired()).isEqualTo(requestUserDetails.isCredentialsNonExpired());
+        assertThat(userDetails.isEnabled()).isEqualTo(requestUserDetails.isEnabled());
+
         verify(userRepository, times(1))
                 .findByEmail(username);
 
     }
-
     @Test
     @DisplayName("Exception")
     void whenUsernameIsNotFound_thenThrowException(){
-        String username = null;
+        String username = "test";
 
         when(userRepository.findByEmail(username))
                 .thenThrow(UsernameNotFoundException.class);
 
-        assertThrows(UsernameNotFoundException.class, ()->{
+        Throwable throwable = assertThrows(UsernameNotFoundException.class, ()->{
             userDetailsServiceImpl.loadUserByUsername(username);
         });
+
         verify(userRepository, times(1))
                 .findByEmail(username);
+
+        assertEquals("Exception", UsernameNotFoundException.class, throwable.getClass());
     }
 }
