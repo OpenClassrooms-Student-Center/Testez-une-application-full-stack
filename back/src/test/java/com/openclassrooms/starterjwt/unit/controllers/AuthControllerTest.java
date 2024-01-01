b2a3e10b704b92e4dbef80d4018824c504d4f2d0
Younes Lahouiti <loller763@gmail.com>
2024-01-01 14:26:22 +0100

    Addition of some tags in the AuthControllerTest class

--- back/src/test/java/com/openclassrooms/starterjwt/unit/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/unit/controllers/AuthControllerTest.java
@@ -6,9 +6,14 @@ import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.request.SignupRequest;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.security.jwt.AuthTokenFilter;
 import com.openclassrooms.starterjwt.security.jwt.JwtUtils;
 
 import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,6 +21,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.OngoingStubbing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration.WebFluxConfig;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,6 +34,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -38,13 +46,18 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 @SpringBootTest
+@WebAppConfiguration
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
-@WebAppConfiguration
+@DisplayName("Auth controller test: api/auth")
 class AuthControllerTest {
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
     @InjectMocks
     private AuthController authController;
@@ -68,12 +81,34 @@ class AuthControllerTest {
 
     private ObjectMapper objectMapper;
 
-    AuthControllerTest() {
+    static private Instant startedAt;
+
+    @BeforeAll
+    static public void initStartingTime() {
+        logger.info("Before all the test suites");
+
+        startedAt = Instant.now();
+    }
+
+    @AfterAll
+    static public void showTestDuration() {
+        logger.info("After all the test suites");
+
+        Instant endedAt = Instant.now();
+        long duration = Duration.between(startedAt, endedAt).toMillis();
+
+        logger.info(MessageFormat.format("Durée des tests : {0} ms", duration));
+    }
+
+    public AuthControllerTest() {
+        // this.webApplicationContext = new WebApplicationContext();
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         this.objectMapper = new ObjectMapper();
     }
 
     @Test
+    @Tag("post_api/auth/login")
+    @DisplayName("(HAPPY PATH) it should authenticate the user successfully and return a JWT")
     void authenticateUser_shouldReturnJwtResponse() throws Exception {
         // Arrange
         LoginRequest loginRequest = new LoginRequest();
@@ -107,6 +142,8 @@ class AuthControllerTest {
     }
 
     @Test
+    @Tag("post_api/auth/register")
+    @DisplayName("(HAPPY PATH) it should register the user")
     void registerUser_shouldReturnMessageResponse() throws Exception {
         // Arrange
         SignupRequest signupRequest = new SignupRequest();
@@ -115,12 +152,6 @@ class AuthControllerTest {
         signupRequest.setLastName("Doe");
         signupRequest.setEmail("test@example.com");
         signupRequest.setPassword("password");
-
-        // Mock the userRepository.existsByEmail() method
-        when(userRepository.existsByEmail(any())).thenReturn(false);
-
-        // Mock the passwordEncoder.encode() method
-        when(passwordEncoder.encode(any())).thenReturn("encodedPassword");
 
         // Act
         ResultActions result = mockMvc.perform(post("/api/auth/register")
