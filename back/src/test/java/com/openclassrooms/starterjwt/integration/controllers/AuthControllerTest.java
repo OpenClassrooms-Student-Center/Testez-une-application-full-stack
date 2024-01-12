a49a300b87d4c5e21ceb86b5f84c24e49da4d669
Younes Lahouiti <loller763@gmail.com>
2024-01-12 14:22:24 +0100

    Addition of a successful integration test for the auth controller

--- back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/AuthControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/controllers/AuthControllerTest.java
@@ -1,5 +1,114 @@
 package com.openclassrooms.starterjwt.integration.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.security.services.UserDetailsImpl;
+import com.openclassrooms.starterjwt.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AuthControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    /**
+     * Mocked password encoder for encoding and decoding passwords.
+     */
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @Test
+    // Test to register and login a new user
+    public void testRegisterAndLoginWithValidCredentials() throws Exception {
+        // * Act
+        String email = "test@test.com";
+        String password = "test!1234";
+
+        SignupRequest signUpRequest = new SignupRequest();
+
+        signUpRequest.setEmail(email);
+        signUpRequest.setLastName("Tester");
+        signUpRequest.setFirstName("Tester");
+        signUpRequest.setPassword(password);
+
+        // Create new user's account
+        User user = User.builder()
+                .id(10L)
+                .email(signUpRequest.getEmail())
+                .firstName(signUpRequest.getFirstName())
+                .lastName(signUpRequest.getLastName())
+                .password(signUpRequest.getPassword()).build();
+
+        // Prepare a login request
+        UserDetailsImpl userDetails = UserDetailsImpl.builder()
+                .id(10L)
+                .firstName("Tester")
+                .lastName("Tester")
+                .username(email)
+                .password(password)
+                .build();
+
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail(email);
+        loginRequest.setPassword(password);
+
+        // Stubbing the UserRepository behavior
+        when(userRepository.save(user)).thenReturn(null);
+
+        // Stubbing the AuthenticationManager behavior
+        // UserDetails userDetails = mock(UserDetails.class);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(new UsernamePasswordAuthenticationToken(userDetails, null)); // userDetails is your mocked
+
+        // Perform the registration request
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signUpRequest)))
+                .andExpect(status().isOk());
+
+        // Perform the login request
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk());
+    }
 }
