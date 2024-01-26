cac85cee4115367fffcf17ccad3f2a191b7b61a9
Younes Lahouiti <loller763@gmail.com>
2024-01-26 14:32:19 +0100

    Initialization of the E2E tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,6 +1,8 @@
+/// <reference types="Cypress" />
+
 describe('Login spec', () => {
   it('Login successfull', () => {
-    cy.visit('/login')
+    cy.visit('/login');
 
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -8,20 +10,23 @@ describe('Login spec', () => {
         username: 'userName',
         firstName: 'firstName',
         lastName: 'lastName',
-        admin: true
+        admin: true,
       },
-    })
+    });
 
     cy.intercept(
       {
         method: 'GET',
         url: '/api/session',
       },
-      []).as('session')
+      []
+    ).as('session');
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
 
-    cy.url().should('include', '/sessions')
-  })
+    cy.url().should('include', '/sessions');
+  });
 });
