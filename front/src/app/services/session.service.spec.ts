7a8c495a0779d00875666a9e1c6e24a707ef99d9
Frederic Guyont <you@example.com>
2024-02-08 16:39:40 +0100

    ADD: SessionService unit tests

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,8 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
-describe('SessionService', () => {
+describe('SessionService unit tests', () => {
   let service: SessionService;
 
   beforeEach(() => {
@@ -13,5 +14,18 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should log in the user', () => {
+    let sessionInformation: SessionInformation = { token: 'token', type: 'Bearer', id: 1, username: 'yoga@studio.com', firstName: 'Admin', lastName: 'Admin', admin: true };
+    service.logIn(sessionInformation);
+    expect(service.sessionInformation).toEqual(sessionInformation);
+    expect(service.isLogged).toBeTruthy();
+  });
+
+  it('should log out the user', () => {
+    service.logOut();
+    expect(service.sessionInformation).toEqual(undefined);
+    expect(service.isLogged).toBeFalsy();
   });
 });
